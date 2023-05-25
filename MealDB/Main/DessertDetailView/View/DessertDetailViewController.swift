29308d6c14e2abf2ab717e1bb8bb28e43819aecf
Adam Smith <Smittysadam@gmail.com>
2023-05-25 11:34:13 -0500

    created detail storyboard.

---
+++ MealDB/Main/DessertDetailView/View/DessertDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeDetailsViewController.swift
+//  MealDB
+//
+//  Created by Adam Smith on 5/25/23.
+//
+
+import UIKit
+
+class DessertDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
