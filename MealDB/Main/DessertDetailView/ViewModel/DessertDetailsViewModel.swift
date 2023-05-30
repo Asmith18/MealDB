8d9d4f5626cf40029634e483bea288b41bc12261
Adam Smith <Smittysadam@gmail.com>
2023-05-30 15:16:02 -0500

    finished project.

--- MealDB/Main/DessertDetailView/ViewModel/DessertDetailsViewModel.swift
+++ MealDB/Main/DessertDetailView/ViewModel/DessertDetailsViewModel.swift
@@ -24,28 +24,28 @@ class DessertDetailsViewModel {
     }
     
     func fetchDessertDetails(completion: @escaping (RecipeDetails, URL?) -> Void) {
-            guard let id = recipe?.id else { return }
-            
-            service.fetchDessertDetails(from: .dessertById(id)) { [weak self] result in
-                switch result {
-                case .success(let dessert):
-                    self?.recipeDetails = dessert.recipe[0]
-                    self?.updateViews(completion: completion)
-                    self?.delegate?.dessertDetailHasData()
-                case .failure(let error):
-                    print(error)
-                }
+        guard let id = recipe?.id else { return }
+        
+        service.fetchDessertDetails(from: .dessertById(id)) { [weak self] result in
+            switch result {
+            case .success(let dessert):
+                self?.recipeDetails = dessert.recipe[0]
+                self?.updateViews(completion: completion)
+                self?.delegate?.dessertDetailHasData()
+            case .failure(let error):
+                print(error)
             }
+        }
+    }
+    
+    private func updateViews(completion: @escaping (RecipeDetails, URL?) -> Void) {
+        guard let recipeDetails = recipeDetails else { return }
+        
+        guard let image = recipeDetails.recpieImage, let url = URL(string: image) else {
+            completion(recipeDetails, nil)
+            return
         }
         
-        private func updateViews(completion: @escaping (RecipeDetails, URL?) -> Void) {
-            guard let recipeDetails = recipeDetails else { return }
-            
-            guard let image = recipeDetails.recpieImage, let url = URL(string: image) else {
-                completion(recipeDetails, nil)
-                return
-            }
-            
-            completion(recipeDetails, url)
-        }
+        completion(recipeDetails, url)
+    }
 }
